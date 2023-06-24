654181f282465403fdef0e0abd9e30f25932bfc3
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-06-24 16:21:47 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+// console.log(btnStart);
+let timerId = null;
+btnStart.addEventListener('click', onClick);
+btnStop.addEventListener('click', offClick);
+function onClick(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', 'true');
+}
+function offClick(event) {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
